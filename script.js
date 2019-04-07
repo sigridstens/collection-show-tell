062606ec08c8882fa32a6ace1ba5e3f7e62b2519
Sigrid Stensvold <sigridstens@gmail.com>
2019-04-07 12:12:23 -0500

    Created couch collection as data array of POJOs within Vue instance

---
+++ script.js
@@ -0,0 +1,35 @@
+//Collection of couch POJOs
+
+var myCouchCollection = new Vue({
+    el: "#house",
+    data: {
+        couches: [
+            {   name: "Grandma's Couch",
+                squishyFactor: 7,
+                color: "leaf green",
+                purchased: false,
+                age: 50
+            },
+
+            {   name: "Kevin's Couch",
+                squishyFactor: 11,
+                color: "camel",
+                purchased: false,
+                age: 25
+            },
+
+            {   name: "Porch Couch",
+                squishyFactor: 8,
+                color: "olive",
+                purchased: true,
+                age: 50
+            },
+
+            {   name: "Future Deck Couch",
+                squishyFactor: 3,
+                color: "TBD",
+                age: -1
+            }
+        ]
+    }
+});
