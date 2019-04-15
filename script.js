0d90d83a3831583cd8a283380492d8ea7d01cd8f
Sigrid Stensvold <sigridstens@gmail.com>
2019-04-14 19:43:08 -0500

    Added delete functionality and id property to objects

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 //Collection of couch POJOs
 new Vue({
     el: "#mainSection",
+    methods: {
+        deleteCouch: function (couchObject) {
+            this.collection = this.collection.filter(function (couch){
+                return couch.id !== couchObject.id;
+            })
+        }
+    },
     data: {
         collection: [
             {   name: "Grandma's Couch",
@@ -8,7 +15,8 @@ new Vue({
                 color: "leaf green",
                 booPurchased: false,
                 age: 50,
-                imgRoute: "img/grandmacouch.jpg"
+                imgRoute: "img/grandmacouch.jpg",
+                id: 1
             },
 
             {   name: "Kevin's Couch",
@@ -16,7 +24,8 @@ new Vue({
                 color: "camel",
                 booPurchased: false,
                 age: 25,
-                imgRoute: "img/kevin-couch.jpg"
+                imgRoute: "img/kevin-couch.jpg",
+                id: 2
             },
 
             {   name: "Porch Couch",
@@ -24,7 +33,8 @@ new Vue({
                 color: "olive",
                 booPurchased: true,
                 age: 50,
-                imgRoute: "img/porch-couch.jpg"
+                imgRoute: "img/porch-couch.jpg",
+                id: 3
             },
 
             {   name: "Future Deck Couch",
@@ -32,7 +42,8 @@ new Vue({
                 color: "TBD",
                 booPurchased: null,
                 age: -1,
-                imgRoute: "https://i.pinimg.com/originals/4b/18/d5/4b18d577913fddacf32268e456287427.jpg"
+                imgRoute: "https://i.pinimg.com/originals/4b/18/d5/4b18d577913fddacf32268e456287427.jpg",
+                id: 4
 
             }
         ]
